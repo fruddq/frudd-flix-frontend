29afac1f6dedfa9c31b3e8a43d495c19fc44ae1b
Frudd <giadi735@gmail.com>
2023-02-19 11:01:13 +0100

    Seems good

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,23 @@
 export const Footer: React.FunctionComponent = () =>
 (
-    <>
-        <footer className="footer">
-            <ul className="footer-ul">
-                <li className="footer-li">&copy; Giang Dinh 2023</li>
-                <li className="footer-li">giadi735@gmail.com</li>
-                <li className="footer-li">
-                    <a href="https://www.linkedin.com/in/giang-dinh-99686ba6/" className="footer-link">
-                        LinkedIn
-                    </a>
-                </li>
-                <li className="footer-li">
-                    <a href="https://github.com/fruddq" className="footer-link">
-                        github
-                    </a>{" "}
-                </li>
-            </ul>
-        </footer>
-    </>
+  <>
+    <footer className="footer">
+      <ul className="footer-ul">
+        <li className="footer-li">&copy; Giang Dinh 2023</li>
+        <li className="footer-li">giadi735@gmail.com</li>
+        <li className="footer-li">
+          <a href="https://www.linkedin.com/in/giang-dinh-99686ba6/" className="footer-link">
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer-li">
+          <a href="https://github.com/fruddq" className="footer-link">
+            github
+          </a>{" "}
+        </li>
+      </ul>
+    </footer>
+  </>
 )
 
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom"
 
 export const Header: React.FunctionComponent = () =>
 (
-    <div className="header">
-        <Link to="/movies/1">  <img className="frudd-flix-logo" src={logo} alt="frudd-flix logo" /></Link>
-        <Menu />
-    </div>
+  <div className="header">
+    <Link to="/movies/1">  <img className="frudd-flix-logo" src={logo} alt="frudd-flix logo" /></Link>
+    <Menu />
+  </div>
 )
 
